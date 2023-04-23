a7683bcdebe7cbd98607c0c0d25ba80e851b3816
shawnpi <shawnpi@qq.com>
2023-04-24 02:23:27 +0800

    update commit message

---
+++ git_push.sh
@@ -0,0 +1,4 @@
+git add .                     //添加所有文件到暂存区
+git status                    //查看状态
+git commit -m 'update commit message'      //把文件提交到仓库 -m 后面的是备注信息
+git push                      //推送至远程仓库
