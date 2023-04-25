65b548d30f67bfe53c2efea4e94fff75f2cfb9b2
shawnpi <shawnpi@qq.com>
2023-04-25 21:28:58 +0800

    update audios

--- git_push.sh
+++ git_push.sh
@@ -1,4 +1,7 @@
 git add .                     //添加所有文件到暂存区
+sleep 1s
 git status                    //查看状态
+sleep 1s
 git commit -m 'update commit message'      //把文件提交到仓库 -m 后面的是备注信息
+sleep 1s
 git push                      //推送至远程仓库
